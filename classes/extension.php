0bb653c42ce089532b201f340e22fc47a9a5010d
Stéphane HULARD <s.hulard@chstudio.fr>
2016-09-05 11:42:49 +0200

    Create xml\extension class

---
+++ classes/extension.php
@@ -0,0 +1,50 @@
+<?php
+namespace mageekguy\atoum\xml;
+
+use mageekguy\atoum;
+use mageekguy\atoum\observable;
+use mageekguy\atoum\runner;
+use mageekguy\atoum\test;
+
+class extension implements atoum\extension
+{
+    public function __construct(atoum\configurator $configurator = null)
+    {
+        if ($configurator)
+        {
+            $parser = $configurator->getScript()->getArgumentsParser();
+            $handler = function($script, $argument, $values) {
+                $script->getRunner()->addTestsFromDirectory(dirname(__DIR__) . '/tests/units/classes');
+            };
+            $parser
+                ->addHandler($handler, array('--test-ext'))
+                ->addHandler($handler, array('--test-it'))
+            ;
+        }
+    }
+
+    public function setRunner(runner $runner)
+    {
+        return $this;
+    }
+
+    public function setTest(test $test)
+    {
+        $asserter = null;
+        $test->getAssertionManager()
+            ->setHandler(
+                'xml',
+                function($xml, $depth = null, $options = null) use ($test, & $asserter) {
+                    if ($asserter === null)
+                    {
+                        $asserter = new atoum\xml\asserters\xml($test->getAsserterGenerator());
+                    }
+                    return $asserter->setWith($xml, $depth, $options);
+                }
+            )
+        ;
+        return $this;
+    }
+
+    public function handleEvent($event, observable $observable) {}
+}
