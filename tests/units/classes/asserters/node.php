fdc7a78cb32e61e63d5483afbb99fd34aee6ea15
Stéphane HULARD <s.hulard@chstudio.fr>
2016-09-06 12:27:53 +0200

    Add a more tests on node/nodes asserters

--- tests/units/classes/asserters/node.php
+++ tests/units/classes/asserters/node.php
@@ -37,19 +37,43 @@ class node extends atoum\test
 
     public function testHasNamespaceFail()
     {
-        $string = $this->realdom->regex('/[a-z]+/');
         $this
             ->given(
-                $test = $this
+                $test = $this,
+                $xml = new \SimpleXmlElement('<?xml version="1.0"?><root></root>'),
+                $prefix = 'm',
+                $uri = 'http://example.com'
             )
             ->if($asserter = new testedClass())
+            ->and($asserter->setWith($xml))
             ->then
-                ->exception(function() use ($asserter, & $value, $test, $string) {
-                        $asserter->setWith($value = $test->sample($string));
+                ->exception(function() use ($asserter, & $value, $test, $prefix, $uri) {
+                        $asserter->hasNamespace($prefix, $uri);
                     }
                 )
                     ->isInstanceOf('mageekguy\atoum\asserter\exception')
-                    ->hasMessage(sprintf('%s is not a valid XML', $value))
+                    ->hasMessage(sprintf('%s namespace does not exists with URI: %s', $prefix, $uri))
+        ;
+    }
+
+    public function testHasDocNamespaceFail()
+    {
+        $this
+            ->given(
+                $test = $this,
+                $xml = new \SimpleXmlElement('<?xml version="1.0"?><root></root>'),
+                $prefix = 'm',
+                $uri = 'http://example.com'
+            )
+            ->if($asserter = new testedClass())
+            ->and($asserter->setWith($xml))
+            ->then
+                ->exception(function() use ($asserter, & $value, $test, $prefix, $uri) {
+                        $asserter->hasDocNamespace($prefix, $uri);
+                    }
+                )
+                    ->isInstanceOf('mageekguy\atoum\asserter\exception')
+                    ->hasMessage(sprintf('%s document namespace does not exists with URI: %s', $prefix, $uri))
         ;
     }
 
@@ -114,6 +138,67 @@ XML;
                 ->item(0)
                     ->nodeValue
                         ->isEqualTo('Node content')
+        ;
+    }
+
+    public function testXpathWithNamespace()
+    {
+        $xml = <<<XML
+<?xml version="1.0"?>
+<root xmlns:m="http://example.com">
+    <node>Node content</node>
+    <m:node>
+        <subnode m:attr="value" />
+    </m:node>
+</root>
+XML;
+
+        $xml = $this->xml($xml);
+
+        $xml
+            ->xpath('//node')
+                ->hasSize(1)
+        ;
+        $xml
+            ->withNamespace('m', 'http://example.com')
+            ->xpath('//m:node')
+                ->hasSize(1)
+                ->first()
+                ->xpath('./subnode/@m:attr')
+                    ->hasSize(1)
+                    ->item(0)
+                        ->nodeValue
+                            ->isEqualTo('value')
+        ;
+    }
+
+    public function testAttributes()
+    {
+        $xml = <<<XML
+<?xml version="1.0"?>
+<root xmlns:m="http://example.com">
+    <node attr1="value 1" m:attr2="value 2">Node content</node>
+    <node>
+        <subnode />
+    </node>
+</root>
+XML;
+
+        $item = $this
+            ->xml($xml)
+            ->hasDocNamespace('m', 'http://example.com')
+            ->children
+                ->item(0)
+        ;
+        $item
+            ->attributes('m')
+                ->hasSize(1)
+                ->string['attr2']->isEqualTo('value 2')
+        ;
+        $item
+            ->attributes('http://example.com')
+                ->hasSize(1)
+                ->string['attr2']->isEqualTo('value 2')
         ;
     }
 }
--- tests/units/classes/asserters/nodes.php
+++ tests/units/classes/asserters/nodes.php
@@ -16,4 +16,22 @@ class nodes extends atoum\test
                 ->isSubClassOf('mageekguy\atoum\asserter')
         ;
     }
+
+    public function testSetWithInvalid()
+    {
+        $string = $this->realdom->regex('/[a-z]+/');
+        $this
+            ->given(
+                $test = $this
+            )
+            ->if($asserter = new testedClass())
+            ->then
+                ->exception(function() use ($asserter, & $value, $test, $string) {
+                        $asserter->setWith($value = $test->sample($string));
+                    }
+                )
+                    ->isInstanceOf('mageekguy\atoum\asserter\exception')
+                    ->hasMessage(sprintf('%s is not a valid array of SimpleXMLElement', var_export($value, true)))
+        ;
+    }
 }
