d6e85e8105924ea4c01de13753120f3845c8f58f
Stéphane HULARD <s.hulard@chstudio.fr>
2016-10-26 17:32:58 +0100

    Finalize unit tests on `extension` class

--- tests/units/classes/extension.php
+++ tests/units/classes/extension.php
@@ -3,12 +3,11 @@
 namespace mageekguy\atoum\xml\tests\units;
 
 use mageekguy\atoum;
-use mageekguy\atoum\xml\extension as testedClass
-;
+use mageekguy\atoum\xml\extension as SUT;
 
 class extension extends atoum\test
 {
-    public function testClass()
+    public function test_class()
     {
         $this
             ->testedClass
@@ -26,29 +25,29 @@ class extension extends atoum\test
         $configurator = new \mock\mageekguy\atoum\configurator($script);
         $this
             ->then
-                ->object($extension = new testedClass())
+                ->object($extension = new SUT())
             ->if($this->resetMock($parser))
-            ->if($extension = new testedClass($configurator))
+            ->if($extension = new SUT($configurator))
             ->then
                 ->mock($parser)
-                    ->call('addHandler')->twice()
+                    ->call('addHandler')->once()
         ;
     }
 
-    public function testSetRunner()
+    public function test_set_runner()
     {
         $this
-            ->if($extension = new testedClass())
+            ->if($extension = new SUT())
             ->and($runner = new atoum\runner())
             ->then
                 ->object($extension->setRunner($runner))->isIdenticalTo($extension)
         ;
     }
 
-    public function testSetTest()
+    public function test_set_handler()
     {
         $this
-            ->if($extension = new testedClass())
+            ->if($extension = new SUT())
             ->and($test = new \mock\mageekguy\atoum\test())
             ->and($manager = new \mock\mageekguy\atoum\test\assertion\manager())
             ->and($test->setAssertionManager($manager))
@@ -57,8 +56,27 @@ class extension extends atoum\test
                 ->mock($manager)
                     ->call('setHandler')->withArguments('xml')->once()
         ;
+    }
 
-        $faker = $test->xml('<?xml version="1.0" ?><root></root>');
-        $this->object($faker)->isInstanceOf('mageekguy\atoum\xml\asserters\node');
+    public function test_xml_asserter_with_value()
+    {
+        $asserter = $this->then->xml('<?xml version="1.0" ?><root></root>');
+        $this
+            ->then
+                ->object($asserter)
+                    ->isInstanceOf('mageekguy\atoum\xml\asserters\node')
+        ;
+    }
+
+    public function test_xml_asserter_without_value()
+    {
+        $this
+            ->then
+                ->exception(function () {
+                    $this->then->xml();
+                })
+                    ->isInstanceOf('mageekguy\atoum\exceptions\logic')
+                    ->hasMessage('Xml is undefined')
+        ;
     }
 }
