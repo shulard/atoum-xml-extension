db627f48c1bc40895bb133fd460c66c0102456cf
Stéphane HULARD <s.hulard@chstudio.fr>
2017-02-27 14:21:51 +0100

    Fix CS

--- classes/extension.php
+++ classes/extension.php
@@ -31,46 +31,46 @@ class extension implements atoum\extension
     public function setTest(test $test)
     {
         $asserter = null;
-        $test->getAssertionManager()
-            ->setHandler(
-                'xml',
-                function ($xml = null, $depth = null, $options = null) use ($test, & $asserter) {
-                    if ($asserter === null) {
-                        $asserter = new atoum\xml\asserters\node($test->getAsserterGenerator());
-                    }
-                    if (null === $xml) {
-                        throw new atoum\exceptions\logic("XML is undefined");
-                    }
-
-                    return $asserter
-                        ->setWithTest($test)
-                        ->setWith($xml, $depth, $options);
+        $manager = $test->getAssertionManager();
+        $manager->setHandler(
+            'xml',
+            function ($xml = null, $depth = null, $options = null) use ($test, & $asserter) {
+                if ($asserter === null) {
+                    $asserter = new atoum\xml\asserters\node($test->getAsserterGenerator());
                 }
-            )
-            ->setHandler(
-                'html',
-                function ($html = null, $depth = null, $options = null) use ($test, & $asserter) {
-                    if ($asserter === null) {
-                        $asserter = new atoum\xml\asserters\node($test->getAsserterGenerator());
-                    }
-                    if (null === $html) {
-                        throw new atoum\exceptions\logic("HTML is undefined");
-                    }
-
-                    $internalErrors = libxml_use_internal_errors(true);
-                    $disableEntities = libxml_disable_entity_loader(true);
-                    $dom = new \DOMDocument('1.0', 'UTF-8');
-                    $dom->validateOnParse = true;
-                    @$dom->loadHTML($html);
-                    libxml_use_internal_errors($internalErrors);
-                    libxml_disable_entity_loader($disableEntities);
-
-                    return $asserter
-                        ->setWithTest($test)
-                        ->setWith($dom, $depth, $options);
+                if (null === $xml) {
+                    throw new atoum\exceptions\logic("XML is undefined");
                 }
-            )
-        ;
+
+                return $asserter
+                    ->setWithTest($test)
+                    ->setWith($xml, $depth, $options);
+            }
+        );
+        $manager->setHandler(
+            'html',
+            function ($html = null, $depth = null, $options = null) use ($test, & $asserter) {
+                if ($asserter === null) {
+                    $asserter = new atoum\xml\asserters\node($test->getAsserterGenerator());
+                }
+                if (null === $html) {
+                    throw new atoum\exceptions\logic("HTML is undefined");
+                }
+
+                $internalErrors = libxml_use_internal_errors(true);
+                $disableEntities = libxml_disable_entity_loader(true);
+                $dom = new \DOMDocument('1.0', 'UTF-8');
+                $dom->validateOnParse = true;
+                @$dom->loadHTML($html);
+                libxml_use_internal_errors($internalErrors);
+                libxml_disable_entity_loader($disableEntities);
+
+                return $asserter
+                    ->setWithTest($test)
+                    ->setWith($dom, $depth, $options);
+            }
+        );
+
         return $this;
     }
 
